7f5132a5f725a3314e667d3fbe63a56d3786aef7
Vasil Ivanov <vasil.ivanov3@bosch.io>
2024-09-30 10:54:43 +0300

    some minor refactoring and make the fields for min and max optional

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ SCOPE = "playlist-modify-public playlist-modify-private"
 SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
 SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_API_URL = "https://api.spotify.com/v1"
+RECOMMENDATIONS_URL = f"{SPOTIFY_API_URL}/recommendations"
+CLIENT_AUTH_URL = f"{SPOTIFY_AUTH_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}"
 
 
 @app.route("/")
@@ -24,8 +26,7 @@ def home():
 
 @app.route("/login")
 def login():
-    auth_url = f"{SPOTIFY_AUTH_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPE}"
-    return redirect(auth_url)
+    return redirect(CLIENT_AUTH_URL)
 
 
 @app.route("/callback")
@@ -40,6 +41,7 @@ def callback():
             "client_id": CLIENT_ID,
             "client_secret": CLIENT_SECRET,
         },
+        timeout=5,
     )
 
     response_data = response.json()
@@ -49,50 +51,73 @@ def callback():
     return redirect(url_for("home"))
 
 
+# do  some input validation and prepare the request params
+def construct_recommendations_params(form):
+    genres = form["genre"].replace(" ", "").lower()
+    min_tempo = form.get("min_tempo", None)
+    max_tempo = form.get("max_tempo", None)
+    target_tempo = int(form["target_tempo"])
+    max_num_songs = int(form["max_num_songs"])
+
+    min_tempo = int(min_tempo) if min_tempo else None
+    max_tempo = int(max_tempo) if max_tempo else None
+
+    if min_tempo is not None and max_tempo is not None:
+        if min_tempo == max_tempo:
+            min_tempo = max_tempo - 1  # prevents empty responses by giving some leeway
+        elif min_tempo > max_tempo:
+            min_tempo, max_tempo = (
+                max_tempo,
+                min_tempo,
+            )  # swap if min_tempo is greater than max_tempo
+
+    params = {}
+    params["seed_genres"] = genres
+    if min_tempo:
+        params["min_tempo"] = min_tempo
+    if max_tempo:
+        params["max_tempo"] = max_tempo
+
+    params["target_tempo"] = target_tempo
+    params["limit"] = max_num_songs
+
+    return params
+
+
 @app.route("/generate_playlist", methods=["POST"])
 def generate_playlist():
     access_token = session.get("access_token")
-
-    genre = request.form["genre"].replace(" ", "").lower()
-    min_tempo = int(request.form["min_tempo"])
-    max_tempo = int(request.form["max_tempo"])
-    target_tempo = int(request.form["target_tempo"])
-    max_num_songs = int(request.form["max_num_songs"])
 
     headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json",
     }
 
-    recommendations_url = f"{SPOTIFY_API_URL}/recommendations"
-    if min_tempo == max_tempo:
-        min_tempo = target_tempo - 1  # prevents empty responses by giving some leeway
-
-    params = {
-        "seed_genres": genre,
-        "min_tempo": min_tempo,
-        "max_tempo": max_tempo,
-        "target_tempo": target_tempo,
-        "limit": max_num_songs,
-    }
-
-    response = requests.get(recommendations_url, headers=headers, params=params)
+    # prepare the recommendations
+    recommendations_params = construct_recommendations_params(request.form)
+    response = requests.get(
+        RECOMMENDATIONS_URL, headers=headers, params=recommendations_params, timeout=5
+    )
     recommendations = response.json()
     track_uris = [track["uri"] for track in recommendations["tracks"]]
 
-    user_profile = requests.get(f"{SPOTIFY_API_URL}/me", headers=headers).json()
-    user_id = user_profile["id"]
-
+    # prepare the playlist
     playlist_data = {
-        "name": f"Generated Playlist: {genre} - Tempo {target_tempo} BPM",
-        "description": f"A playlist with songs between {min_tempo} and {max_tempo} BPM",
+        "name": f"Generated Playlist: {recommendations_params['seed_genres']} - Tempo {recommendations_params['target_tempo']} BPM",
+        "description": f"A playlist with songs around {recommendations_params['target_tempo']} BPM",
         "public": False,
     }
+
+    user_profile = requests.get(
+        f"{SPOTIFY_API_URL}/me", headers=headers, timeout=5
+    ).json()
+    user_id = user_profile["id"]
 
     playlist_creation_response = requests.post(
         f"{SPOTIFY_API_URL}/users/{user_id}/playlists",
         headers=headers,
         json=playlist_data,
+        timeout=5,
     )
 
     if playlist_creation_response.status_code != 201:
@@ -100,13 +125,16 @@ def generate_playlist():
 
     playlist = playlist_creation_response.json()
 
+    # fill the playlist with the recommendations
     add_tracks_url = f"{SPOTIFY_API_URL}/playlists/{playlist['id']}/tracks"
 
     _add_tracks_response = requests.post(
-        add_tracks_url, headers=headers, json={"uris": track_uris}
+        add_tracks_url, headers=headers, json={"uris": track_uris}, timeout=5
     )
 
-    return render_template("success.html", playlist_url=playlist["external_urls"]["spotify"])
+    return render_template(
+        "success.html", playlist_url=playlist["external_urls"]["spotify"]
+    )
 
 
 if __name__ == "__main__":
